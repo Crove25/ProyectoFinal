7db8fa23c901e0e67fc43c1309af10e65d7a75bd
Franco <71045051+Crove25@users.noreply.github.com>
2020-09-09 19:26:23 -0300

    Initial commit

---
+++ Assets/Scripts/wait_start.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class wait_start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(logo_wait());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator logo_wait()
+    {
+        yield return new WaitForSeconds(3);
+
+        SceneManager.LoadScene("loggin");
+    }
+
+
+}
